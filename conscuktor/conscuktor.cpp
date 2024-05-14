6db81b6a2a89a9e64943d5b795625b2e9173e074
SandyArdiyan <sanardiyan881@gmail.com>
2024-05-14 18:51:11 +0700

    Add project files.

---
+++ conscuktor/conscuktor.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Mahasiswa
+{
+public:
+	int nim;
+	string nama;
+
+public:
+	Mahasiswa()
+	{
+		nim = 0;
+		nama = "";
+	};
+	Mahasisw(int iNim)
+	{
+		nim = iNim;
+
+	};
+	Mahasiswa
